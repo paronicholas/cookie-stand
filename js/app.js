cf6da3c5b91f7f36fd7e8681bd8401344a86b802
Nicholas Paro <nichparo@amazon.com>
2019-06-12 09:32:46 -0700

    created new branch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function setSectionEl(){
+  var sectionEl = document.getElementById('section-container');
+  return sectionEl;
+}
+
+function setUlEl() {
+  var ulEl = document.createElement('ul');
+  return ulEl;
+}
+
+function setLiEl(){
+  var liEl = document.createElement('li');
+  return liEl;
+}
+
+
